890127afb6a30ff3708c5ab0558575aceaf9d9da
sohambiswas314 <157901358+sohambiswas314@users.noreply.github.com>
2024-05-12 18:17:00 +0530

    Add files via upload

---
+++ main1st.py
@@ -0,0 +1,71 @@
+
+# Define a variable
+x = 5
+
+# Print the variable
+print("The value of x is:", x)
+
+
+# Example of practicing basic scripting
+# to perform arithmetic operations,
+# manipulate strings, and use conditional statements
+
+# Arithmetic Operations
+a = 10
+b = 5
+
+# Addition
+sum_result = a + b
+print("Sum:", sum_result)
+
+# Subtraction
+difference = a - b
+print("Difference:", difference)
+
+# Manipulating Strings
+message = "Hello"
+name = "Soham"
+
+# Concatenation
+greeting = message + ", " + name + "!"
+print(greeting)
+
+# Conditional Statements
+x = 15
+
+if x > 10:
+    print("x is greater than 10")
+else:
+    print("x is less than or equal to 10")
+
+# Example of exploring common data structures
+# like lists, dictionaries, and tuples
+
+# Lists
+fruits = ['apple', 'banana', 'orange']
+
+# Accessing elements
+print("First fruit:", fruits[0])
+
+# Adding elements
+fruits.append('grape')
+print("Updated list of fruits:", fruits)
+
+# Dictionaries
+person = {'name': 'Soham', 'age': 21, 'city': 'Kolkata'}
+
+# Accessing values
+print("Name:", person['name'])
+print("Age:", person['age'])
+
+# Adding new key-value pair
+person['job'] = 'Developer'
+print("Updated person info:", person)
+
+# Tuples
+coordinates = (10, 20)
+
+# Accessing elements
+print("X-coordinate:", coordinates[0])
+print("Y-coordinate:", coordinates[1])
+
